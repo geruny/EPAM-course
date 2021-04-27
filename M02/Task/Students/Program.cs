66992ef8d544c068ebe9e3e7028e52e1a1086f00
geruny <gerunuky@gmail.com>
2021-04-27 15:48:17 +0300

    Task #1 done

--- M02/Task/Students/Program.cs
+++ M02/Task/Students/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Students
 {
@@ -10,6 +7,38 @@ namespace Students
     {
         static void Main(string[] args)
         {
+            var subjects = new[] { "Maths", "Geometry", "Computer science", "English", "Physics", "Geography" };
+            var listStudents = new List<Student>
+            {
+                new Student("vasya.pupkin@epam.com"),
+                new Student("Vasya", "Pupkin"),
+
+                new Student("german.petrov@epam.com"),
+                new Student("German", "Petrov"),
+
+                new Student("mikhail.fedorov@epam.com"),
+                new Student("Mikhail", "Fedorov")
+            };
+            var studentSubjectDict = new Dictionary<Student, HashSet<string>>();
+            var rnd = new Random();
+
+            foreach (var student in listStudents)
+                studentSubjectDict[student] = new HashSet<string> {
+                    subjects[rnd.Next(0, subjects.Length - 1)],
+                    subjects[rnd.Next(0, subjects.Length - 1)],
+                    subjects[rnd.Next(0, subjects.Length - 1)]
+                };
+
+            foreach (KeyValuePair<Student, HashSet<string>> keyValue in studentSubjectDict)
+            {
+                Console.Write("\n" + keyValue.Key.ToString() + " - ");
+
+                foreach (var subject in keyValue.Value)
+                    Console.Write(subject + " ");
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
 }
---
+++ M02/Task/Students/Student.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace Students
+{
+    class Student
+    {
+        private string _fullName;
+        private string _email;
+
+        public Student(string email)
+        {
+            _email = email;
+            _fullName = email.Substring(0, email.IndexOf('@'));
+
+            string[] words = _fullName.Split('.');
+            StringBuilder sb = new StringBuilder();
+            foreach (string word in words)
+                sb.Append(char.ToUpper(word[0]) + word.Substring(1, word.Length - 1) + " ");
+
+            _fullName = sb.ToString().Remove(sb.Length - 1);
+        }
+
+        public Student(string name, string surname)
+        {
+            _fullName = name + " " + surname;
+            _email = (name + "." + surname + "@epam.com").ToLower();
+        }
+
+        public override string ToString() => _fullName;
+
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != this.GetType())
+                return false;
+
+            return this.ToString() == obj.ToString();
+        }
+
+        public override int GetHashCode() => this.ToString().GetHashCode();
+    }
+}
