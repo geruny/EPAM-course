35ddac456a0b14992b173acfee31c01658385476
geruny <gerunuky@gmail.com>
2021-05-06 18:32:21 +0300

    geometric shapes done

---
+++ M04/Task/Geometric Shapes/Rectangle.cs
@@ -0,0 +1,11 @@
+﻿namespace Geometric_Shapes
+{
+    internal class Rectangle : Shape
+    {
+        public Rectangle(double x, double y) : base(x, y) { }
+
+        public override void CalculatePerimeter() => Perimeter = 2 * (Y + X);
+
+        public override void CalculateArea() => Area = X * Y;
+    }
+}
---
+++ M04/Task/Geometric Shapes/Square.cs
@@ -0,0 +1,17 @@
+﻿namespace Geometric_Shapes
+{
+    internal class Square : Shape
+    {
+        public Square(double x) : base(x) { }
+
+        public override void CalculatePerimeter()
+        {
+            Perimeter = 4 * X;
+        }
+
+        public override void CalculateArea()
+        {
+            Area = X * X;
+        }
+    }
+}
