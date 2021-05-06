3c9742832a6249de22d1a5a0e14b3ebe5befdb88
geruny <gerunuky@gmail.com>
2021-05-06 18:32:21 +0300

    geometric shapes done

---
+++ M04/Task/Geometric Shapes/Circle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Geometric_Shapes
+{
+    internal class Circle : Shape
+    {
+        private readonly double _radius;
+
+        public Circle(double x) : base(x) => _radius = x;
+
+        public override void CalculatePerimeter()
+        {
+            Perimeter = 2 * Math.PI * _radius;
+        }
+
+        public override void CalculateArea()
+        {
+            Area = Math.PI * Math.Pow(_radius, 2);
+        }
+    }
+}
--- M04/Task/Geometric Shapes/Program.cs
+++ M04/Task/Geometric Shapes/Program.cs
@@ -2,11 +2,30 @@
 
 namespace Geometric_Shapes
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static readonly Rectangle Rec = new(5, 6);
+        private static readonly Circle Circle = new(4);
+        private static readonly Square Square = new(2);
+        private static readonly Triangle Triangle = new(2, 5, 6);
+
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TestShape(Rec);
+            TestShape(Circle);
+            TestShape(Square);
+            TestShape(Triangle);
         }
+
+        private static void TestShape(Shape shape)
+        {
+            PrintL(shape.ToString());
+            shape.CalculatePerimeter();
+            shape.CalculateArea();
+            shape.Print();
+            PrintL();
+        }
+
+        private static void PrintL(string str="") => Console.WriteLine(str);
     }
 }
