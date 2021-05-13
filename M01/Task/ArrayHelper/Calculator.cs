71d6f1935e59b594a600b7ba3d7075879cea2ad5
geruny <gerunuky@gmail.com>
2021-05-13 20:28:56 +0300

    added M01

---
+++ M01/Task/ArrayHelper/Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace ArrayHelper
+{
+    public class Calculator
+    {
+        public static int FindSum2DimArray(int[,] array)
+        {
+            int resultSum = 0;
+            foreach (var i in array)
+                if (i > 0)
+                    resultSum += i;
+
+            return resultSum;
+        }
+    }
+}
