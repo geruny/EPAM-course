4f8fdf2fd3bfb7ff71301348bfcd88a377176f0b
geruny <gerunuky@gmail.com>
2021-05-14 04:02:30 +0300

    unit tests for M03

---
+++ M03/Task/ConsoleApp.Test/StringerTest.cs
@@ -0,0 +1,121 @@
+using System;
+using NUnit.Framework;
+
+namespace ConsoleApp.Test
+{
+    [TestFixture]
+    internal class StringerTest
+    {
+        private readonly char[] _delimiterChars = {' ', ',', '.', ':', '\t', '\n', '(', ')', '/', '@', '#', '!'};
+
+        [Test]
+        public void GetWordsFromString_NullEmptyWhiteSpaceString_ArgumentException([Values(null, "", " ")] string str)
+        {
+            //Act & assert
+            Assert.That(() => Stringer.GetWordsFromString(str, _delimiterChars),
+                Throws.Exception.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void GetWordsFromString_GivenStringWithoutWords_ArgumentException()
+        {
+            //Arrange
+            var str = " / . @#";
+
+            //Act & assert
+            Assert.That(() => Stringer.GetWordsFromString(str, _delimiterChars),
+                Throws.Exception.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void GetWordsFromString_GivenString_WordsArr()
+        {
+            //Arrange
+            var str = "Create a class@ that implements a method!";
+            var expected = new[] {"Create", "a", "class", "that", "implements", "a", "method"};
+
+            //Act
+            var result = Stringer.GetWordsFromString(str, _delimiterChars);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(expected));
+
+            foreach (var s in result)
+                Console.WriteLine(s);
+        }
+
+        [Test]
+        public void GetAverageWordLength_GivenString_AverageWordsLength()
+        {
+            //Arrange
+            var str = "Create a class that implements";
+            var expected = 5.2;
+
+            //Act
+            var result = Stringer.GetAverageWordLength(str);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void DoubleDefChars_NullOrWhiteSpaceStrings_ArgumentException([Values(null, "", " ")] string str, [Values(null, "", " ")] string defChars)
+        {
+            //Act & assert
+            Assert.That(() => Stringer.DoubleDefChars(str, defChars),
+                Throws.Exception.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void DoubleDefChars_String_StringDoubleDefChars()
+        {
+            //Arrange
+            var str = "omg i love shrek";
+            var defChars = "o kek";
+            var expected = "oomg i loovee shreekk";
+
+            //Act
+            var result = Stringer.DoubleDefChars(str, defChars);
+
+            //Assert
+            Assert.That(result,Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void GetSumOfNums_NullOrWhiteSpaceStrings_ArgumentException([Values(null, "", " ")] string str1, [Values(null, "", " ")] string str2)
+        {
+            //Act & assert
+            Assert.That(() => Stringer.GetSumOfNums(str1, str2),
+                Throws.Exception.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void GetSumOfNums_Strings_StringWithSumNums()
+        {
+            //Arrange
+            var str1 = "123";
+            var str2 = "123";
+            var expected = (int.Parse(str1)+int.Parse(str2)).ToString();
+
+            //Act
+            var result = Stringer.GetSumOfNums(str1, str2);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void ReverseString_String_ReverseString()
+        {
+            //Arrange
+            var str = "The greatest victory is that which requires no battle";
+            var expected = "battle no requires which that is victory greatest The";
+
+            //Act
+            var result = Stringer.ReverseString(str);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
